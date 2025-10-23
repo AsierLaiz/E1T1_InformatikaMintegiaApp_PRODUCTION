2b9d438ebaf5a551817ceb43353c32e8103b9eae
Ainara Diaz <adiazm24@fpsanturtzhilh.eus>
2025-10-23 13:21:59 +0200

    empezado inbentarioa

---
+++ public/views/inbentarioa.php
@@ -0,0 +1,27 @@
+<?php require_once "partials/header.php" ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+<div class="contenedor my-5">
+  <h1 class="mb-5">Inbentarioa</h1>
+
+
+
+  <table class=" table  table-hover" id="tabla-inbentarioa">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Izena</th>
+        <th scope="col">Modeloa</th>
+        <th scope="col">Kategoria</th>
+        <th scope="col">Kantitatea</th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      
+    </tbody>
+  </table>
+
+</div>
+<script src="../assets/js/inbentarioa.js"></script>
+<?php require_once "partials/footer.php" ?>
