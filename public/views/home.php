651b8b62430df67d5ed3e84921e8b8f5c7e5827f
asier <apenao24@fpsanturtzilh.eus>
2025-10-23 11:38:57 +0200

    Cambios Diseño y nuevas Vistas

---
+++ public/views/home.php
@@ -0,0 +1,22 @@
+<?php require_once "partials/header.html" ?>
+
+<main>
+    <div class="container-fluid text-white vh-100 d-flex align-items-center justify-content-center p-5 container-home">
+
+        <div class="text-center">
+            <h1 class="display-1 fw-bolder text-uppercase mb-4 title-home">
+                Ongietorri
+            </h1>
+            
+            <h2 class="display-5 fw-light mb-5 subtitle-home">
+                FP Santurtzi LH
+            </h2>
+
+            <a href="./perfil.php" class="btn btn-outline-light btn-lg px-5 py-3 rounded-pill shadow-lg text-decoration-none">
+                Ver Mi Perfil
+            </a>
+        </div>
+    </div>
+</main>
+
+<?php require_once "partials/footer.html" ?>
