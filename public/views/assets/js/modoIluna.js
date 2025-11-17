e37fa18af3cf8a94e55868c1494dd8db433ebe50
ainara <adiazm24@fpsanturtzhilh.eus>
2025-11-17 12:31:20 +0100

    theme botoia eta logoa aldatuta

--- public/views/assets/js/modoIluna.js
+++ public/views/assets/js/modoIluna.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggle = document.getElementById("themeToggle");
+    const logo = document.getElementById("logo");
+
+    const url1 = "./assets/img/logo/FPSanturtzi_Logo.png";
+    const url2 = "./assets/img/logo/FPSanturtzi_Logo_iluna.png";
 
     //ikusten dugu se tema daukan gordeta erabiltzailea
     const themeGordeta = localStorage.getItem("tema");
+
+    //logoa aldatzeko
+    if (themeGordeta === "dark") {
+        logo.src = url2;
+    } 
+    
+    if (themeGordeta === "light") {
+        logo.src = url1;
+    }
 
     if (themeGordeta === "dark") {
         document.body.classList.add("dark-mode");
@@ -14,11 +27,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
             document.body.classList.toggle("dark-mode");
 
-
             if (document.body.classList.contains("dark-mode")) {
                 localStorage.setItem("tema", "dark");
+                logo.src = url2;
+
             } else {
                 localStorage.setItem("tema", "light");
+                logo.src = url1;
             }
         });
     }
